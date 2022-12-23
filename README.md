58b5df1883adffdd22256a092ffcad800df6fa62
AlexieVQ <alexie@nephos.me>
2022-12-24 00:54:08 +0100

    Support variables environnement

--- README.md
+++ README.md
@@ -8,3 +8,4 @@ Mastodon.
 - [`requests`](https://requests.readthedocs.io/en/latest/)
 - [`jsons`](https://github.com/ramonhagenaars/jsons)
 - [`beautifulsoup4`](https://www.crummy.com/software/BeautifulSoup/)
+- [`python-dotenv`](https://github.com/theskumar/python-dotenv)
--- rappelconso/main.py
+++ rappelconso/main.py
@@ -1,29 +1,33 @@
-from argparse import ArgumentParser, Namespace
-import sys
+import os
+
+from dotenv import load_dotenv
 from flux import Flux
 from mastodon import Mastodon
 from draft import Draft
 from cache import Cache
 
 
-def parse_args() -> Namespace:
-    parser = ArgumentParser(
-        prog="RappelConso",
-        description="Bot postant les nouvelles fiches de RappelConso sur " \
-            "Mastodon",
-    )
-    parser.add_argument("-b", "--bearer-token", dest="token",
-        help="Bearer token pour l'API Mastodon (doit avoir les accès " \
-        "write:statuses et write:media)", required=True)
-    parser.add_argument("-c", "--cache", dest="cache", help="Fichier texte " \
-        "où cacher l'historique des fiches traitées", required=True)
-    return parser.parse_args()
+def get_env(nom: str) -> str | None:
+    """Retourne la valeur de la variable de configuration de nom donnée.
+
+    - Si une variable d'environnement NOM_FILE est définie, retourne le contenu
+      du fichier référencé.
+    - Sinon, retourne la valeur de la variable d'environnement NOM.
+    """
+    chemin = os.getenv(nom + "_FILE")
+    if chemin is not None:
+        fichier = open(chemin, "r")
+        val = fichier.readline().strip()
+        fichier.close()
+        return val
+    else:
+        return os.getenv(nom)
 
 if __name__ == "__main__":
-    args = parse_args()
-    cache = Cache(args.cache)
+    load_dotenv()
+    cache = Cache(get_env("RAPPELCONSO_FICHIER_CACHE"))
     flux = Flux("https://data.economie.gouv.fr/api/records/1.0/search/?dataset=rappelconso0&q=&rows=10&sort=date_de_publication&facet=categorie_de_produit&facet=sous_categorie_de_produit&facet=nom_de_la_marque_du_produit&facet=conditionnements&facet=zone_geographique_de_vente&facet=distributeurs&facet=motif_du_rappel&facet=risques_encourus_par_le_consommateur&facet=conduites_a_tenir_par_le_consommateur&facet=modalites_de_compensation&facet=date_de_publication", cache)
-    mastodon = Mastodon("rivals.space", args.token)
+    mastodon = Mastodon("rivals.space", get_env("RAPPELCONSO_BEARER_TOKEN"))
     fiches = flux.update()
     for fiche in fiches:
         mastodon.post(Draft(fiche))
