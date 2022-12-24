c3d81c739782ef042492baabd479453231de5d46
AlexieVQ <alexie@nephos.me>
2022-12-24 13:47:39 +0100

    Boucle infinie mise en place

--- rappelconso/main.py
+++ rappelconso/main.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from time import sleep, time
 
 from dotenv import load_dotenv
 from flux import Flux
@@ -25,7 +26,14 @@ def get_env(nom: str) -> str | None:
     else:
         return os.getenv(nom)
 
-def publier_fiches(fiches: list[Fiche], historique: Historique) -> None:
+def publier_fiches(
+    fiches: list[Fiche],
+    mastodon: Mastodon,
+    historique: Historique,
+) -> None:
+    """Publie les fiches données, avec un intervalle de 15 min entre chaque
+    publication.
+    """
     if len(fiches) == 0:
         logging.info("Aucune nouvelle fiche à publier.")
         return
@@ -34,10 +42,24 @@ def publier_fiches(fiches: list[Fiche], historique: Historique) -> None:
         logging.info("Publication de la fiche %s...",
             fiche.lien_vers_la_fiche_rappel)
         try:
+            start = time()
             mastodon.post(Draft(fiche))
             historique.add(fiche.rappelguid)
+            sleep_duration = 900 - time() + start
+            if sleep_duration > 0:
+                sleep(sleep_duration)
         except Exception as e:
             logging.error("Erreur pendant la publication de la fiche : %s", e)
+
+def run(flux: Flux, mastodon: Mastodon, historique: Historique) -> None:
+    """Exécute le bot, en récupérant les nouvelles fiches toutes les heures."""
+    while True:
+        logging.info("Récupération des nouveaux rappels produits...")
+        start = time()
+        publier_fiches(flux.update(), mastodon, historique)
+        sleep_duration = 3600 - time() + start
+        if sleep_duration > 0:
+            sleep(sleep_duration)
 
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
@@ -45,4 +67,4 @@ if __name__ == "__main__":
     historique = Historique(get_env("RAPPELCONSO_FICHIER_HISTORIQUE"))
     flux = Flux("https://data.economie.gouv.fr/api/records/1.0/search/?dataset=rappelconso0&q=&rows=10&sort=date_de_publication&facet=categorie_de_produit&facet=sous_categorie_de_produit&facet=nom_de_la_marque_du_produit&facet=conditionnements&facet=zone_geographique_de_vente&facet=distributeurs&facet=motif_du_rappel&facet=risques_encourus_par_le_consommateur&facet=conduites_a_tenir_par_le_consommateur&facet=modalites_de_compensation&facet=date_de_publication", historique)
     mastodon = Mastodon("rivals.space", get_env("RAPPELCONSO_BEARER_TOKEN"))
-    publier_fiches(flux.update(), historique)
+    run(flux, mastodon, historique)
