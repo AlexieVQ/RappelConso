abc6b4e9f734cfb8ff7fd8d15bde7bc99b3328d8
AlexieVQ <alexie@nephos.me>
2023-02-16 12:00:10 +0100

    50 fiches récupérées au lieu de 10

--- rappelconso/main.py
+++ rappelconso/main.py
@@ -65,6 +65,6 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
     load_dotenv()
     historique = Historique(get_env("RAPPELCONSO_FICHIER_HISTORIQUE"))
-    flux = Flux("https://data.economie.gouv.fr/api/records/1.0/search/?dataset=rappelconso0&q=&rows=10&sort=date_de_publication&facet=categorie_de_produit&facet=sous_categorie_de_produit&facet=nom_de_la_marque_du_produit&facet=conditionnements&facet=zone_geographique_de_vente&facet=distributeurs&facet=motif_du_rappel&facet=risques_encourus_par_le_consommateur&facet=conduites_a_tenir_par_le_consommateur&facet=modalites_de_compensation&facet=date_de_publication", historique)
+    flux = Flux("https://data.economie.gouv.fr/api/records/1.0/search/?dataset=rappelconso0&q=&rows=50&sort=date_de_publication&facet=categorie_de_produit&facet=sous_categorie_de_produit&facet=nom_de_la_marque_du_produit&facet=conditionnements&facet=zone_geographique_de_vente&facet=distributeurs&facet=motif_du_rappel&facet=risques_encourus_par_le_consommateur&facet=conduites_a_tenir_par_le_consommateur&facet=modalites_de_compensation&facet=date_de_publication", historique)
     mastodon = Mastodon("rivals.space", get_env("RAPPELCONSO_BEARER_TOKEN"))
     run(flux, mastodon, historique)
